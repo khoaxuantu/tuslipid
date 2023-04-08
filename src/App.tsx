95361cd25d485051405d210e80f1f2a204b12bfb
khoaxuantu <68913255+khoaxuantu@users.noreply.github.com>
2023-04-08 18:38:47 +0800

    Add exit animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import * as Site from './components/site';
+import { AnimatePresence } from 'framer-motion';
 
 function App() {
+  const location = useLocation();
+
   return (
-    <Routes>
-      <Route path='/' element={<Site.Layout />}>
-        <Route index element={<Site.MenuBuilder />} />
-        <Route path='about' element={<Site.AboutPage />} />
-        <Route path='projects' element={<Site.ProjectsPage />} />
-        <Route path='blogs' element={<Site.BlogPage />} />
-      </Route>
-    </Routes>
+    <AnimatePresence mode='wait'>
+      <Routes location={location} key={location.pathname}>
+        <Route path='/' element={<Site.Layout />}>
+          <Route index element={<Site.MenuBuilder />} />
+          <Route path='about' element={<Site.AboutPage />} />
+          <Route path='projects' element={<Site.ProjectsPage />} />
+          <Route path='blogs' element={<Site.BlogPage />} />
+        </Route>
+      </Routes>
+    </AnimatePresence>
   );
 }
 
--- src/components/site.tsx
+++ src/components/site.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { motion as m} from "framer-motion";
 import * as Header from "./header_grp";
 import * as Content from "./content_grp";
 import SocialMediaBtnGroup, { NavBtnGroup } from "./button";
@@ -17,27 +18,33 @@ export function Layout() {
 
 export function MenuBuilder() {
     return (
-        <div className="container menu-wrapper center">
+        <m.div
+            transition={{ duration: 1 }}
+            exit={{ opacity: 0 }}
+            className="container menu-wrapper center transition-menu">
             <Header.MenuHeaderGrp />
             <SocialMediaBtnGroup />
             <NavBtnGroup />
             <Copyright />
-        </div>
+        </m.div>
     );
 }
 
 export function AboutPage() {
     return (
-        <div className="container page-wrapper">
+        <m.div
+            transition={{ duration: 1 }}
+            exit={{ opacity: 0 }}
+            className="container page-wrapper transition-page">
             <Header.AboutHeaderGrp />
             <Content.AboutContent />
-        </div>
+        </m.div>
     );
 }
 
 export function ProjectsPage() {
     return (
-        <div className="container page-wrapper">
+        <div className="container page-wrapper transition-page">
             
         </div>
     );
