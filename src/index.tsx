70539daf087eba603eee59bcbc71019199dcaadf
khoaxuantu <68913255+khoaxuantu@users.noreply.github.com>
2023-04-10 03:07:09 +0800

    reconfigure

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
