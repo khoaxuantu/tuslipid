c20c2f626ff45302166c2aa0512e975842e22aed
khoaxuantu <68913255+khoaxuantu@users.noreply.github.com>
2023-04-10 16:13:50 +0800

    Update to static metadata

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,22 @@ import React from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import * as Site from './components/site';
 import { AnimatePresence } from 'framer-motion';
-import { HelmetProvider } from 'react-helmet-async';
-import Landing from './components/landing';
 
 
 function App() {
   const location = useLocation();
-  const helmetcontext = {};
 
   return (
-    <HelmetProvider context={helmetcontext}>
-      <AnimatePresence mode='wait'>
-        <Routes location={location} key={location.pathname}>
-          <Route path='/' element={<Site.Layout />}>
-            <Route index element={<Site.MenuBuilder />} />
-            <Route path='about' element={<Site.AboutPage />} />
-            <Route path='projects' element={<Site.ProjectsPage />} />
-            <Route path='blogs' element={<Site.BlogPage />} />
-          </Route>
-        </Routes>
-      </AnimatePresence>
-      <Landing />
-    </HelmetProvider>
+    <AnimatePresence mode='wait'>
+      <Routes location={location} key={location.pathname}>
+        <Route path='/' element={<Site.Layout />}>
+          <Route index element={<Site.MenuBuilder />} />
+          <Route path='about' element={<Site.AboutPage />} />
+          <Route path='projects' element={<Site.ProjectsPage />} />
+          <Route path='blogs' element={<Site.BlogPage />} />
+        </Route>
+      </Routes>
+    </AnimatePresence>
   );
 }
 
