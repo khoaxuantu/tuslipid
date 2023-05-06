28bc0c1dd1ac4a679f448d98823ceb505f5bf8bc
khoaxuantu <68913255+khoaxuantu@users.noreply.github.com>
2023-05-07 00:52:41 +0800

    Add form and first oauth

--- src/components/guestbook/form.tsx
+++ src/components/guestbook/form.tsx
@@ -1,13 +1,76 @@
-function Form() {
+import { useEffect, useState } from "react";
+
+interface IMessage {
+    name: string | null,
+    message: string
+}
+
+function GuestbookForm() {
+    const [User, setUser] = useState(null);
+
+    // console.log(User);
+
+    async function fetchUser(thirdParty: string) {
+        console.log(`get ${thirdParty} user`);
+        const fetchAPI = await fetch(`http://localhost:3001/oauth/${thirdParty}/user`, {
+            method: "GET",
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem("accessToken")
+            }
+        });
+        const response = await fetchAPI.json();
+        return response;
+    }
+
+    const getGithubUser = async () => {
+        const response = await fetchUser("github");
+        setUser(response.login);
+        localStorage.removeItem("accessToken");
+        localStorage.setItem("data", JSON.stringify({
+            name: response.login
+        }));
+    }
+
+    const getGoogleUser = async () => {
+        const response = await fetchUser("google");
+        setUser(response.name);
+        localStorage.removeItem("accessToken");
+        localStorage.setItem("data", JSON.stringify({
+            name: response.name
+        }));
+    }
+
+    // useEffect(() => {
+    //     if (localStorage.getItem("third-party") === "github") {
+    //         getGithubUser();
+    //     }
+    //     else if (localStorage.getItem("third-party") === "google") {
+    //         getGoogleUser();
+    //     }
+    // }, []);
+
+    async function submitHandler(e: any) {
+        e.preventDefault();
+
+        const input = document.getElementById("your-message") as HTMLFormElement;
+        const newMessage: IMessage = {
+            name: User,
+            message: input.value
+        }
+        
+
+    }
+
     return (
-        <form method="post" action="" className="mb-3 guestbook-form row">
+        <form className="mb-3 guestbook-form row" onSubmit={submitHandler}>
             <input 
                 className="col-10 ps-3 pt-3 pb-3 pe-5" 
                 placeholder="Your message..."
-                type="text" name="" id="" />
+                autoComplete="off"
+                type="text" name="your message" aria-label="your message" id="your-message" />
             <button className="col-2 p-2" type="submit"><b>Sign</b></button>
         </form>
     );
 }
 
-export default Form;
+export default GuestbookForm;
--- src/components/oauth.tsx
+++ src/components/oauth.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 
-export function Oauth() {
+export function GithubOauth() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const codeParam = urlParams.get("code");
@@ -9,13 +9,15 @@ export function Oauth() {
 
     if (codeParam && (localStorage.getItem("accessToken") === null)) {
         const getAccessToken = async function () {
-            await fetch("http://localhost:3001/oauth/getGithubAccessToken?code=" + codeParam, {
+            await fetch("http://localhost:3001/oauth/github/access_token?code=" + codeParam, {
                 method: "GET"
             })
             .then(response => response.json())
             .then(data => {
                 if (data.access_token) {
                     localStorage.setItem("accessToken", data.access_token);
+                    localStorage.setItem("third-party", "github");
+                    localStorage.setItem("action", "signout");
                     return navigate("/guestbook");
                 }
             });
@@ -24,7 +26,7 @@ export function Oauth() {
         getAccessToken();
     }
 
-    return <div></div>;
+    return null;
 }
 
-export default Oauth;
+export default GithubOauth;
