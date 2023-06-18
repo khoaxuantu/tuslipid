ccc59a7fdaf63883a027955a83ba47440c61b9c6
khoaxuantu <68913255+khoaxuantu@users.noreply.github.com>
2023-06-18 17:07:01 +0700

    Update copyright

--- src/components/copyright.tsx
+++ src/components/copyright.tsx
@@ -3,17 +3,17 @@ import { BsGithub } from "react-icons/bs";
 function Copyright(props: { copyright_class?: string }) {
     const year = new Date().getFullYear();
     return (
-        <div className={`${props.copyright_class} pt-4`}>
+        <div className={`${props.copyright_class}`}>
             <div className="proj-github-ref">
                 <a href="https://github.com/khoaxuantu/tuslipid" target="_blank" rel="noreferrer" aria-label="web source code">
                     <BsGithub opacity={0.8} color="black" />
                 </a>
             </div>
-            Created by Xuan Khoa Tu Nguyen with {" "}
-            <a href="https://react.dev/">React</a>, {" "}
-            <a href="https://www.typescriptlang.org/">TypeScript</a> and {" "}
-            <a href="https://sass-lang.com/">Sass</a> {" "}
-            © {year}.
+            Built and designed by Xuan Khoa Tu Nguyen with {" "}
+            <a href="https://react.dev/" target="_blank" rel="noreferrer">React</a>, {" "}
+            <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer">TypeScript</a> and {" "}
+            <a href="https://sass-lang.com/" target="_blank" rel="noreferrer">Sass</a>.<br></br>
+            Copyright © {year} All Rights Reserved.
         </div>
     );
 }
