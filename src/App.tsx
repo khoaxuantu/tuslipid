d74087cf48a3c3928b019ff3c0d2bbd46ab4d37b
khoaxuantu <68913255+khoaxuantu@users.noreply.github.com>
2023-08-20 00:21:38 +0700

    Optimize code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { lazy } from 'react';
+import { Suspense, lazy } from 'react';
 import { Route, Routes, useLocation, Navigate } from 'react-router-dom';
 import MainLayout from './components/layouts/main';
 import BlogLayout from './components/layouts/blog';
 import MenuBuilder from './components/menu/page';
 import AboutPage from './components/about/page';
 import ProjectsPage from './components/projects/page';
-import { AnimatePresence } from 'framer-motion';
 import ScrollToTop from './components/scrollToTop';
 import NotFound from './components/404';
+
 
 const Oauth = lazy(() => import('./components/oauth'));
 const BlogsPage = lazy(() => import('./components/blogs/listpage'));
@@ -19,7 +19,7 @@ function App() {
   const location = useLocation();
 
   return (
-    <AnimatePresence mode='wait'>
+    <>
       <ScrollToTop />
       <Routes location={location} key={location.pathname}>
         <Route path='/' element={<MainLayout />}>
@@ -27,16 +27,32 @@ function App() {
           <Route path='about' element={<AboutPage />} />
           <Route path='projects' element={<ProjectsPage />} />
           <Route path='blogs' element={<BlogLayout />}>
-            <Route index element={<BlogsPage />} />
-            <Route path=':blogId' element={<SingleBlogPage />} />
+            <Route index element={<Suspense fallback={<BlogsPageLoader />}><BlogsPage /></Suspense>} />
+            <Route path=':blogId' element={<Suspense fallback={<SingleBlogPageLoader />}><SingleBlogPage /></Suspense>} />
           </Route>
-          <Route path='guestbook' element={<GuestbookPage />} />
-          <Route path='oauth' element={<Oauth />} />
+          <Route path='guestbook' element={<Suspense><GuestbookPage /></Suspense>} />
+          <Route path='oauth' element={<Suspense><Oauth /></Suspense>} />
           <Route path='404' element={<NotFound />} />
         </Route>
         <Route path='*' element={<Navigate to="/404" replace={true} />} />
       </Routes>
-    </AnimatePresence>
+    </>
+  );
+}
+
+function BlogsPageLoader() {
+  return (
+    <div style={{
+      marginTop: 1000
+    }}></div>
+  );
+}
+
+function SingleBlogPageLoader() {
+  return (
+    <div style={{
+      height: 1000
+    }}></div>
   );
 }
 
