f455be98fae212b9f9ad41faa8c68a23ef0f00e5
khoaxuantu <ngxktuzkai2000@gmail.com>
2025-08-26 21:15:20 +0700

    feat: add dynamic param generator to blogs

--- src/app/(navbar)/blogs/[blog_id]/page.tsx
+++ src/app/(navbar)/blogs/[blog_id]/page.tsx
@@ -8,19 +8,29 @@ interface DynamicParams {
   params: Promise<{ blog_id: string }>;
 }
 
+export function generateStaticParams() {
+  return Object.values(BLOG_INFO_DICT).map((blog) => {
+    return {
+      blog_id: blog.url.slice(1),
+    };
+  });
+}
+
+export const dynamicParams = false;
+
 export default async function SingleBlogPage(props: DynamicParams) {
   const params = await props.params;
   const id = params.blog_id.replaceAll("-", "_");
-  if (!BLOG_INFO_DICT[id]) notFound();
+  if (!BLOG_INFO_DICT[id]) return notFound();
 
   return (
     <>
-      <SingleBlogPageHeaderGrp id={id as string} />
+      <SingleBlogPageHeaderGrp id={id} />
       <hr></hr>
       <main>
         <article className="blog-l-wrapper row">
           <TableOfContents />
-          <SingleBlogContent id={id as string} />
+          <SingleBlogContent id={id} />
         </article>
       </main>
     </>
